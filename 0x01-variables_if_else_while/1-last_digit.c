06829a372a8282ab96a23923e8cdbc2232c8e4f9
najadams <najmadams1706@gmail.com>
2023-03-19 12:05:20 -0700

    corrected the syntax error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -29,9 +29,9 @@ int main(void)
 	{
 		printf("%d is %d and is 0 \n", n, last);
 	}
-	else if (last <= 6 && last != 0)
+	else if (last < 6 && last != 0) 
 	{
-		printf("%d is %d and is greater than 5\n", n, last);
+		printf("%d is %d and is less than 6 and not 0\n", n, last);
 	}
 	return (0);
 }
