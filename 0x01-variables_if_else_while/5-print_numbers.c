c4f12858e686f613fe2c0fa8e8209f2d8562a35e
najadams <najmadams1706@gmail.com>
2023-03-19 16:24:01 -0700

    excluded 10 from the iteration

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* your code goes there */
+	int a = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
