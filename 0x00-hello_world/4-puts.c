9c532a8b1cf0c15afdd04fa47b60a88f65f6f883
najadams <najmadams1706@gmail.com>
2023-03-19 08:43:04 -0700

    added the required parenthesis for betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*/
+ * main - Entry point
+ *
+ * Return: 0 on success, non-zero on error
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingua puzzle");
-	return 0;
+	return (0);
 }
