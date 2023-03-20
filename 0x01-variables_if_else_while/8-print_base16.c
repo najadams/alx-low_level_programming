47ed0bfc8b1c77a8372a8d536066bc6b3e029046
najadams <najmadams1706@gmail.com>
2023-03-20 15:45:38 -0700

    addint file to print hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* your code goes there */
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		c = i + '0';
+		putchar(c);
+	}
+	for (i = 10; i < 16; i++)
+	{
+		c = i - 10 + 'a';
+		putchar(c);
+	}
+	return (0);
+}
