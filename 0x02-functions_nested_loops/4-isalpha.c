d308a04223b81fd714018df13267f97c1fad45c1
najadams <najmadams1706@gmail.com>
2023-03-22 04:29:11 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+
+/**
+ * _islower(int c) tells if a character is lower
+ *
+ * Return: Always 0.
+ */
+int  _isalpha(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
