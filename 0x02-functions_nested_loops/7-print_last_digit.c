0a8d44d7ed147da378b11f2e0387b8dc0326e894
najadams <najmadams1706@gmail.com>
2023-03-22 07:35:52 -0700

    printing last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints  the last value
+ * @num: this is a parameter
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int a)
+{
+	int r;
+
+	r = a % 10;
+	return (r);
+}
