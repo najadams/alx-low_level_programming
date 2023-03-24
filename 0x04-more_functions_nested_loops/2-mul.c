1d7ac168714b834a4e4ee4f88f70fddc41b1adeb
najadams <najmadams1706@gmail.com>
2023-03-23 22:52:21 -0700

    printing the product of 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * mul- return the product of two numbers
+ * @b : stores one of the variable
+ * @a : this is the variable to store input
+ * Return: the value of the product
+ */
+
+int mul(int a, int b)
+{
+	int ans;
+
+	ans = a * b;
+	return (ans);
+}
