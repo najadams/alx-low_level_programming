75d3ad595aaf0983b33056aeb100b82d3bf7408c
najadams <najmadams1706@gmail.com>
2023-03-27 15:49:40 -0700

    completed task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* reset_to_98 - updates the value using pointer
+* @n: stores the address of the pointer
+*
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+working with the memory in C
