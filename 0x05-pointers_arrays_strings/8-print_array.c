77a1d71b5ae1358f6123d6c1902c1e760ac5274a
najadams <najmadams1706@gmail.com>
2023-03-28 15:15:43 -0700

    completed task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+* print_array - prints half end of the string
+* @a: stores the address of the pointer
+* @n: stores the number 
+*
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n-1)
+		{
+			printf(", ");
+		}
+	}
+}
