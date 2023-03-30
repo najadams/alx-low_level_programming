4bdabb494953408ca2f26c3706bce4b9b1fb34c9
najadams <najmadams1706@gmail.com>
2023-03-30 09:36:37 -0700

    completed task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * string_toupper- changes to uppercase
+ * @str: stores the variable
+ * Return: this returns the string
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
