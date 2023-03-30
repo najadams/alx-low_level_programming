d100e6162ced211748279938968a1818d87b0d1e
najadams <najmadams1706@gmail.com>
2023-03-30 07:46:40 -0700

    fixed betty in task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,8 +1,8 @@
 #include <string.h>
 /**
- * *_strcmp - this joins two srcings
+ * reverse_array - this reverses arrays
  * @a: stores the beginning of the new srcing
- * @s2: stores the number
+ * @n: this sets the number to be printed
  * Return: returns the ascii difference
  */
 void reverse_array(int *a, int n)
@@ -10,7 +10,7 @@ void reverse_array(int *a, int n)
 	int i = 0;
 	int temp;
 
-	for (i = 0; i < n/2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
 		a[i] = a[n - 1 - 0];
