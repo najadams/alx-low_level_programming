b08dbfc827d61f30c12a840157fb6c27a5abdd62
najadams <najmadams1706@gmail.com>
2023-03-30 05:21:43 -0700

    fixed betty style in code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,11 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    size_t dest_len = strlen(dest);
-    size_t i;
+	size_t dest_len = strlen(dest);
+	size_t i;
 
-    for (i = 0; src[i] != '\0'; i++)
-            dest[dest_len + i] = src[i];
-    dest[dest_len + i] = '\0';
-    return (dest);
+	for (i = 0; src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return (dest);
 }
