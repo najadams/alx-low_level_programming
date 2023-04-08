5874435e0000f0275438e3f1bc62ed53e6964a6a
najadams <najmadams1706@gmail.com>
2023-04-08 13:48:18 -0700

    printing number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc : stores the number of terminal entry
+ * @argv : stores the strings of input
+ * Return: Always return 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)*argv;
+	printf("%d\n", argc);
+	return (0);
+}
