e379919ca09e260d73f4f0309bb5f087ffb03c40
najadams <najmadams1706@gmail.com>
2023-04-08 13:51:54 -0700

    trying to fix errors

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - prints the name of the program
  * @argc : stores the number of terminal entry
  * @argv : stores the strings of input
  * Return: Always return 0
  */
-
 int main(int argc, char **argv)
 {
-	(void)*argv;
+	(void) *argv;
 	printf("%d\n", argc);
 	return (0);
 }
