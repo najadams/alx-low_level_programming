759d3adb1e1b134cc3a8ea88e38fa9ddfc435078
najadams <najmadams1706@gmail.com>
2023-04-10 21:42:00 -0700

    made changes

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Loop through all .c files in the current directory
-for file in *.c; do
-    # Extract the filename without extension
-    filename="${file%.c}"
+# Get all the .c files in the current directory
+c_files=$(find . -type f -name "*.c")
 
-    gcc "$file" -o "$filename.o"
+# Create a dummy main function to satisfy the linker
+echo "int main() { return 0; }" > dummy_main.c
+
+# Compile each .c file into .o object files
+for c_file in $c_files; do
+  gcc -c $c_file -o ${c_file%.c}.o
 done
+
+
+# Create the static library
 gcc -static -o liball.a *.o
