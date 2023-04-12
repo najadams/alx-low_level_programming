c78024babbe6d9e259339424089600216e32e30b
najadams <najmadams1706@gmail.com>
2023-04-11 17:43:11 -0700

    completed task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of chars
+ * @size : stores the size of the created arrays
+ * @c : stores the char to be inserted in array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr = (char *) malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return (arr);
+}
