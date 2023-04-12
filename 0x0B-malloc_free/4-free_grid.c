1de425ccbb4f1a432e474ecc67eb7dc08ecc3ab8
najadams <najmadams1706@gmail.com>
2023-04-11 22:54:37 -0700

    completed task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - does a lot
+ * @grid : stores width
+ * @height : stores heigt
+ */
+void free_grid(int **grid, int height) {
+    int i;
+
+    for (i = 0; i < height; i++) {
+        free(grid[i]);
+    }
+    free(grid);
+}
